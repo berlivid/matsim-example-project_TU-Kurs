445a5f0da4f48c9b2a8043623ae94202a3a4ff7d
DavidBerling <davidberling@web.de>
2026-07-30 14:56:46 +0200

    setup netowrk und plans files für die scenarien

--- src/main/java/org/matsim/project/RunMatsimApplication.java
+++ src/main/java/org/matsim/project/RunMatsimApplication.java
@@ -33,7 +33,7 @@ import org.matsim.core.controler.OutputDirectoryHierarchy.OverwriteFileSetting;
 public class RunMatsimApplication extends MATSimApplication {
 
 	public RunMatsimApplication() {
-		super("scenarios/munich_base_2023/configBase.xml");
+		super("scenarios/munich_base_2023/config_base.xml");
 	}
 
 	public static void main(String[] args) {
